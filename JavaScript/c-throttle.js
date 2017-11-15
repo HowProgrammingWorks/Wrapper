598206393a092b6d65125c0d3a48b947dd83a8ba
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-15 18:17:07 +0200

    Add throttle implementation

---
+++ JavaScript/c-throttle.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Function throttling, executed once per interval
+
+const throttle = (timeout, fn, ...args) => {
+  let timer;
+  let wait = false;
+  let wrapped = null;
+
+  const throttled = (...par) => {
+    timer = undefined;
+    if (wait) wrapped(...par);
+  };
+
+  wrapped = (...par) => {
+    if (!timer) {
+      timer = setTimeout(throttled, timeout, ...par);
+      wait = false;
+      return fn(...args.concat(par));
+    } else {
+      wait = true;
+    }
+  };
+
+  return wrapped;
+};
+
+// Usage
+
+const fn = (...args) => {
+  console.log('Function called, args: ' + args);
+};
+
+const ft = throttle(200, fn, 'value1');
+
+const timer = setInterval(() => {
+  fn('value2');
+  ft('value3');
+}, 50);
+
+setTimeout(() => {
+  clearInterval(timer);
+}, 2000);
